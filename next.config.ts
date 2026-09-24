723c3af1e515b88e2bac85273c5bda6945b9616d
Apurav Khare <apurav.khare@gmail.com>
2026-09-23 19:03:52 -0700

    try fixing broken images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ONLY_PRODUCTION === 'production' || process.env.NODE_ENV === 'production';
+const repoName = 'dusk-collector';
+
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Required for GitHub Pages static hosting
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true, // GitHub Pages does not support Next.js dynamic image optimization backend
+  },
 };
 
 export default nextConfig;
