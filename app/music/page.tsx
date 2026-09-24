b636f18b7f7b1e5b5dfcaa75e8efebd951bf2231
Apurav Khare <apurav.khare@gmail.com>
2026-09-23 19:29:40 -0700

    add music page

---
+++ app/music/page.tsx
@@ -0,0 +1,11 @@
+export default function MusicPage() {
+  return (
+    <div className="flex items-center justify-center gap-2 dark:bg-black dark:text-zinc-50 min-h-screen">
+      <div className="flex flex-col">
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHkxMnhmcHBkN3Z5cnlrZGphajFvZXVxampzenA0MTFsb2MzdmNhOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/10YpWPBU7GAYwM/giphy.gif"
+          alt="Music League Logo"
+          className="w-64 h-64" />
+      </div>
+    </div>
+  );
+}
