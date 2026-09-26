7b6494919aea50404b2321de1d9247d053e90f3e
Apurav Khare <apurav.khare@gmail.com>
2026-09-26 11:25:38 -0700

    use swiper's parallax

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,11 @@
 import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import type { Swiper as SwiperClass } from 'swiper'
-import { Mousewheel, Pagination } from 'swiper/modules';
+import { Parallax, Mousewheel, Pagination } from 'swiper/modules';
 import About from './about/page';
 import Music from './music/page';
 import ShowsPage from './shows/page';
 import TopNavBar from './TopNavBar';
-import { Parallax, Background } from 'react-parallax';
 import backgroundImage from "../public/ferry_sunset.jpeg";
 import duskCollectorLogo from '../public/Dusk Collector Cutout.svg';
 
@@ -23,9 +22,9 @@ function VideoPage() {
 
 function ParallaxBackground() {
   return (
-    <Parallax bgImage={backgroundImage.src} strength={500} blur={{min: -1, max: 2}} className="w-full h-full flex items-center justify-center object-cover">
-        <img src={duskCollectorLogo.src} className="opacity-40 w-50 h-auto translate-x-[50%]" alt="Dusk Collector Logo" />
-    </Parallax>
+    <div style={{ backgroundImage: `url(${backgroundImage.src})` }} data-swiper-parallax="-300" className="w-full h-full flex items-center justify-center bg-cover bg-center">
+        <img data-swiper-parallax="-400" src={duskCollectorLogo.src} className="opacity-40 w-50 h-auto" alt="Dusk Collector Logo" />
+    </div>
   );
 }
 
@@ -43,7 +42,7 @@ export default function Home() {
     <main className="w-screen h-screen overflow-hidden">
       <TopNavBar swiperInstance={swiperInstance} />
 
-      <Swiper modules={[Mousewheel, Pagination]} direction="vertical" mousewheel={{ forceToAxis: true }} pagination={{ clickable: true }} onSwiper={setSwiperInstance} className="w-full h-full">
+      <Swiper modules={[Parallax, Mousewheel, Pagination]} parallax={true} direction="vertical" mousewheel={{ forceToAxis: true }} pagination={{ clickable: true }} onSwiper={setSwiperInstance} className="w-full h-full">
         {/* Next.js "Pages" loaded as performant components */}
         <SwiperSlide><ParallaxBackground /></SwiperSlide>
         <SwiperSlide><Music /></SwiperSlide>
